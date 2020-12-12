b351d02883f213131d33a657ff9fae5650914d9b
bsamm <brandonc.sammons@gmail.com>
2020-12-11 22:55:48 -0800

    a little logic and some notes

--- elasticsearchapp.py
+++ elasticsearchapp.py
@@ -8,37 +8,48 @@ class ElasticSearchApp(object):
             "about": "About",
             "start": "Start Server",
             "stop": "Stop Server",
-            "port": "View Port 9200",
-            "logs": "View Logs"
+            "port": "Open Port 9200",
+            "logs": "Open Logs Directory"
         }
         self.app = rumps.App(self.config["app_name"])
         self.set_up_menu()
-        self.about_button      = rumps.MenuItem(title=self.config["about"], callback=self.about_elastic_search_app)
         self.start_button      = rumps.MenuItem(title=self.config["start"], callback=self.start_elastic_search)
-        self.quit_button       = rumps.MenuItem(title=self.config["stop"], callback=self.quit_elastic_search)
+        self.stop_button       = rumps.MenuItem(title=self.config["stop"], callback=self.stop_elastic_search)
         self.view_port_button  = rumps.MenuItem(title=self.config["port"], callback=self.view_port_in_browser)
         self.logs_button       = rumps.MenuItem(title=self.config["logs"], callback=self.view_logs)
-        self.app.menu          = [self.about_button, self.start_button, self.quit_button, self.view_port_button, self.logs_button]
+        self.about_button      = rumps.MenuItem(title=self.config["about"], callback=self.about_elastic_search_app)
+        self.app.menu          = [self.start_button, self.stop_button, self.view_port_button, self.logs_button, self.about_button]
+        self.check_status_of_elastic_search()
 
     def set_up_menu(self):
         self.app.title = "ES"
 
-    def about_elastic_search_app(self, sender):
-        rumps.alert("Install elastic search services using homebrew by following the instructions here: https://www.elastic.co/guide/en/elasticsearch/reference/current/brew.html")
-
     def start_elastic_search(self, sender):
         os.system('brew services start elasticsearch')
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
+        self.check_status_of_elastic_search()
 
-    def quit_elastic_search(self, sender):
+    def stop_elastic_search(self, sender):
         os.system('brew services stop elasticsearch')
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
+        self.check_status_of_elastic_search()
+
+    def check_status_of_elastic_search(self):
+        if 'stopped' in os.popen('brew services list | grep elasticsearch').read():
+            self.stop_button.set_callback(None)
+            self.view_port_button.set_callback(None)
+            self.start_button.set_callback(self.start_elastic_search)
+        else:
+            self.start_button.set_callback(None)
+            self.stop_button.set_callback(self.stop_elastic_search)
+            self.view_port_button.set_callback(self.view_port_in_browser)
 
     def view_port_in_browser(self, sender):
         os.system('open http://localhost:9200/')
 
     def view_logs(self, sender):
         os.system('open /usr/local/var/log/elasticsearch')
+
+    def about_elastic_search_app(self, sender):
+        rumps.alert("Install elastic search services using homebrew by following the instructions here: https://www.elastic.co/guide/en/elasticsearch/reference/current/brew.html")
 
     def run(self):
         self.app.run()
